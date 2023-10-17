4e555f2a911ff580f3f34eb9efc1efcc20d95288
20B1NUM2317 MOLOR <20B1NUM2317@stud.num.edu.mn>
2023-10-18 00:54:55 +0800

    header component nemj uguv

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './Header.css';
+export default function Header (){
+    return (<div>
+    <header>
+        <ul>        
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+        </ul>
+
+    </header>
+</div>)
+}
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import Header from './components/Header/Header';
+import * as React from 'react';
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Header/>
+      </div>
+    </main>
+  )
+}
